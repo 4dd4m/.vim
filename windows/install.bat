96960bc8edbaf72562805f84d193d18a6c3706b2
4dd4m <torok-a@ulster.ac.uk>
2022-04-06 07:28:56 +0100

    _vimrc fix

--- windows/install.bat
+++ windows/install.bat
@@ -1,5 +1,5 @@
 choco install ./packages/.config
-copy ..\vimrc C:%HOMEPATH%\vimfiles\_vimrc
+copy ..\vimrc C:%HOMEPATH%\_vimrc
 mkdir C:%HOMEPATH%\vimfiles
 mkdir C:%HOMEPATH%\vimfiles\autoload
 copy plug.vim C:%HOMEPATH%\vimfiles\autoload
